f342124a6f3d6daff43475cf2361fbc2a1307706
Jake Cariello <jakecariello@gmail.com>
2024-11-06 19:54:13 -0500

    feat: simple coming soon page with Bulma styles

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       semi: ['error', 'never'],
+      indent: ['error', 2],
     },
   }
 );
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import sun from './assets/sun.horizon.fill.svg'
+import musicNote from './assets/music.quarternote.3.svg'
+import person2 from './assets/person.2.fill.svg'
+import terminal from './assets/apple.terminal.svg'
+
 import './App.css'
 
 function App() {
   return (
-    <>
-      <a href='https://react.dev' target='_blank'>
-        <img src={sun} className='logo react' alt='Sun rising horizon' />
-      </a>
-      <h1>Coming soon!</h1>
-    </>
+    <div className="container is-flex is-flex-direction-column is-align-items-center
+    is-justify-content-center" style={{ minHeight: '100vh' }}>
+      
+      <div className="is-size-2 mb-6">Coming soon!</div>
+      
+      <div className="columns">
+        <div className="column">
+          <figure className='image is-64x64'>
+            <img src={musicNote} alt="Music note" />
+          </figure>
+        </div>
+        <div className="column">
+          <figure className='image is-64x64'>
+            <img src={person2} alt="Music note" />
+          </figure>
+        </div>
+        <div className="column">
+          <figure className='image is-64x64'>
+            <img src={terminal} alt="Music note" />
+          </figure>
+        </div>
+      </div>
+    </div>
   )
 }
 
