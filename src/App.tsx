e1e218f95eeef1fd09c79286ffaed83e51da885a
Jake Cariello <jakecariello@gmail.com>
2024-11-07 17:36:10 -0500

    feat: little react spring icon spring

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import { Flex, Text } from '@radix-ui/themes'
 import { MdOutlineCode, MdOutlineMusicNote, MdOutlineGroup } from 'react-icons/md'
+import { animated, useTrail } from '@react-spring/web'
+import { useCallback } from 'react'
+
+const ICONS = [MdOutlineCode, MdOutlineMusicNote, MdOutlineGroup]
+const COLORS = ['#AEC6CF', '#F0E68C', '#FFB6C1']
 
 function App() {
+  
+  const [trail, api] = useTrail<{x: number}>(ICONS.length, () => ({
+    from: { x: 0 },
+    to: { x: 1 },
+    config: { tension: 170, friction: 50 },
+  }), [])
+  
+  const handleClick = useCallback(() => {
+    api.set({ x: 0 })
+    api.start({ x: 1 })
+  }, [api])
+
   return (
-    <Flex gap={'5'} direction="column" align="center" justify="center" style={{ height: '100vh' }}>
-      
+    <Flex onClick={handleClick} gap="5" direction="column" align="center" justify="center" style={{ height: '100vh' }}>
       <Text size="6">Coming soon!</Text>
-      
-      <Flex gap={'5'} direction="row">
-        <MdOutlineMusicNote size={64} />
-        <MdOutlineGroup size={64} />
-        <MdOutlineCode size={64} />
+      <Flex gap="5" direction="row">
+        {trail.map(({ x }, index) => {
+          const Icon = ICONS[index]
+          return (
+            <animated.div key={index} style={{
+              transform: x.to(x => `rotate(${x * 360}deg)`),
+              color: x.to([0, 0.5, 1], ['#FFFFFF', COLORS[index], '#FFFFFF'])
+            }}>
+              <Icon size={64} />
+            </animated.div>
+          )
+        })}
       </Flex>
     </Flex>
   )
