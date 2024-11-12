d04f5b576da3bac3e07a9bdce9dadaf00846be16
Jake Cariello <jakecariello@gmail.com>
2024-11-12 17:59:53 -0500

    feat: refactor application structure and replace App component with DefaultScreen (with gradient background and animated icons)

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import '@radix-ui/themes/styles.css'
 import { Theme } from '@radix-ui/themes'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Theme
-      accentColor="mint"
-      grayColor="gray"
-      panelBackground="translucent"
-      scaling="100%"
-      radius="full"
-      appearance='dark'
-    >
-      <App />
-    </Theme>
-  </StrictMode>,
-)
+import { DefaultScreen } from './screens'
+
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <Theme
+        accentColor='mint'
+        grayColor='gray'
+        panelBackground='translucent'
+        scaling='100%'
+        radius='full'
+        appearance='dark'
+      >
+        <DefaultScreen />
+      </Theme>
+    </StrictMode>
+  )
+}
