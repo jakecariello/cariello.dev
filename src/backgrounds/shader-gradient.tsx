9f77702ed0190b43b75d4c5afa2c1fca3d5d9a95
Jake Cariello <jakecariello@gmail.com>
2024-12-12 18:08:13 -0500

    minor fix

--- src/backgrounds/shader-gradient.tsx
+++ src/backgrounds/shader-gradient.tsx
@@ -10,8 +10,8 @@ export default function ShaderGradientBackground() {
         position: 'fixed',
         top: 0,
         left: 0,
-        width: '100%',
-        height: '100%',
+        width: '100vw',
+        height: '100vh',
         backgroundColor: 'dark', // Replace with your desired background color or image
         zIndex: -1, // Ensure it stays behind other content
         pointerEvents: 'none', // prevent interaction with the background
